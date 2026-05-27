8e43c97f83b3157eafacac4f637f7cdb83351930
siddharthsahoo502-boop <siddharthsahoo502@gmail.com>
2026-05-27 18:01:17 +0530

    Update 02_dimensions_exploration.sql

--- scripts/02_dimensions_exploration.sql
+++ scripts/02_dimensions_exploration.sql
@@ -5,11 +5,6 @@ Dimensions Exploration
 ===============================================================================
 Purpose:
     - To explore the structure of dimension tables.
-	
-SQL Functions Used:
-    - DISTINCT
-    - ORDER BY
-===============================================================================
 */
 
 -- Retrieve a list of unique countries from which customers originate
