13ff05829ffdaa3fc63c5e9dc5dd8616db3b3412
siddharthsahoo502-boop <siddharthsahoo502@gmail.com>
2026-05-27 17:59:12 +0530

    Update 01_database_exploration.sql

--- scripts/01_database_exploration.sql
+++ scripts/01_database_exploration.sql
@@ -1,16 +1,3 @@
-/*
-===============================================================================
-Database Exploration
-===============================================================================
-Purpose:
-    - To explore the structure of the database, including the list of tables and their schemas.
-    - To inspect the columns and metadata for specific tables.
-
-Table Used:
-    - INFORMATION_SCHEMA.TABLES
-    - INFORMATION_SCHEMA.COLUMNS
-===============================================================================
-*/
 
 -- Retrieve a list of all tables in the database
 SELECT 
